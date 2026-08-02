cc1185c6e4aa10a8840208688061127e1b5327aa
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-02 13:20:30 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,57 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.trie
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+
+        curr.is_end = True
+        
+
+
+
+    def search(self, word: str) -> bool:
+
+        curr = self.trie
+
+        def dfs(idx, curr):
+            if idx == len(word):
+                return curr.is_end
+            
+            if word[idx] == ".":
+                for child in curr.children:
+                    if dfs(idx + 1, curr.children[child]):
+                        return True
+
+                return False
+
+            else:
+                if word[idx] in curr.children:
+                    return dfs(idx + 1, curr.children[word[idx]])
+
+                return False
+
+
+        return dfs(0, curr)
+
+        
+
+
+
+
+
+
+
+        
