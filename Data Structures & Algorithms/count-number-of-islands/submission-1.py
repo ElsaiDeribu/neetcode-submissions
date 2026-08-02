2002a449aa0e6e705b4306e3c2e6d32e5df3be20
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-02 13:51:05 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+
+        # TC = O(m·n)
+        # SC = O(m·n)
+        
+        count = 0
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+        def is_inbound(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+        def check(row, col):
+            grid[row][col] = "0"
+
+            for dr, dc in dirs:
+                new_row = dr + row
+                new_col = dc + col
+
+                if is_inbound(new_row, new_col) and grid[new_row][new_col] == "1": 
+                    check(new_row, new_col)
+
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "1":
+                    count  += 1
+                    check(row, col)
+
+
+        return count
+
+
+
