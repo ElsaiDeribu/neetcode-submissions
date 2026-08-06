6944dc81a9aef14a99db713ca47e604d72d177c0
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-06 15:05:15 -0400

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+
+        # TC: O(m * n)
+        # SC: O(m * n)
+
+        deq = deque([])
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+        def is_inbound(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 2:
+                    deq.append((row, col))
+
+        ans = -1 if deq else 0
+
+        while deq:
+            
+            for _ in range(len(deq)):
+                row, col = deq.popleft()
+
+                for dr, dc in dirs:
+                    new_row = dr + row
+                    new_col = dc + col
+
+                    if is_inbound(new_row, new_col) and grid[new_row][new_col] == 1:
+                        grid[new_row][new_col] = 2
+                        deq.append((new_row, new_col))
+            
+            ans += 1
+
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    return -1
+
+
+        return ans
+
+
+
