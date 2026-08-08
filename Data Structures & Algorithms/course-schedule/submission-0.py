79b78c2c552e684ce25c1bd5cecbd883819ef259
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-08 12:37:46 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        # TC: O(V + E)
+        # SC: O(V + E)
+
+        pre_count = defaultdict(int)
+        pre_list = defaultdict(list)
+        deq = deque([])
+        order = []
+
+
+        for crs, pre in prerequisites:
+            pre_count[crs] += 1
+            pre_list[pre].append(crs)
+
+
+        for crs in range(numCourses):
+            if pre_count[crs] == 0:
+                deq.append(crs)
+
+        while deq:
+
+            pre = deq.popleft()
+            order.append(pre)
+
+            for crs in pre_list[pre]:
+                pre_count[crs] -= 1
+                if pre_count[crs] == 0:
+                    deq.append(crs)
+
+
+        return len(order) == numCourses
+
+
+                    
+
+
+        
