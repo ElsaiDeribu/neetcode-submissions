70378b8ca6eb1654c68a3859f06032ea69e4cb97
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-10 18:09:48 -0400

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,50 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+
+            
+        #  TC O(M * N)
+        #  SC O(M * N) from recursion stack
+
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+        def is_inbound(row, col):
+            return 0 <= row < len(board) and 0 <= col < len(board[0])
+
+
+        def mark(row, col):
+
+            board[row][col] = "T"
+
+            for dr, dc in dirs:
+                new_row = dr + row
+                new_col = dc + col
+                if is_inbound(new_row, new_col) and board[new_row][new_col] == "O":
+                    mark(new_row, new_col)
+
+
+        for row in [0, len(board) - 1]:
+            for col in range(len(board[0])):
+                if board[row][col] == "O":
+                    mark(row, col)
+
+        for row in range(len(board)):
+            for col in [0, len(board[0]) - 1]:
+                if board[row][col] == "O":
+                    mark(row, col)
+
+
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == "O":
+                    board[row][col] = "X"
+                elif board[row][col] == "T":
+                    board[row][col] = "O"
+
+
+
+
+
+
+
+
+
+        
