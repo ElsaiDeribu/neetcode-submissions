2b9a997f6b8ae85a619cd39034a37189b33872ca
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-11 13:13:32 -0400

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,58 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
+        # TC: O(M * N)
+        # SC: O(M * N)
+
+        visited = set()
+        dirs = [(0,1), (1,0), (0,-1), (-1,0)]
+        def is_inbound(row, col):
+            return 0 <= row < len(heights) and 0 <= col < len(heights[0])
+
+        
+        def dfs(row, col):
+            
+            visited.add((row, col))
+
+            for dr, dc in dirs:
+                new_row = dr + row
+                new_col = dc + col
+
+                if is_inbound(new_row, new_col) and (new_row, new_col) not in visited:
+                    if heights[new_row][new_col] >= heights[row][col]:
+                        dfs(new_row, new_col)
+
+
+
+        for row in range(len(heights)):
+            col = len(heights[0]) - 1
+            if (row, col) not in visited:
+                dfs(row, col)
+
+        for col in range(len(heights[0])):
+            row = len(heights) - 1
+            if (row, col) not in visited:
+                dfs(row, col)
+
+
+        atlantic = visited.copy()
+        visited = set()
+
+        for col in range(len(heights[0])):
+            row = 0
+            if (row, col) not in visited:
+                dfs(row, col)
+
+        for row in range(len(heights)):
+            col = 0
+            if (row, col) not in visited:
+                dfs(row, col)
+
+        
+        return [[row,col] for row, col in visited if (row, col) in atlantic]
+
+
+
+
+
+
