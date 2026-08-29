4939776586d540c4b1c1fa39886360ef8ae629bd
Elsai Shumi <91674971+ElsaiDeribu@users.noreply.github.com>
2026-08-29 19:54:21 -0400

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        # TC: O(n)
+        # SC: O(1)
+        
+        running_min = 0
+        running_sum = 0
+        ans = float("-inf")
+
+        for num in nums:
+            running_sum += num
+            ans = max(ans, running_sum - running_min )
+            running_min = min(running_sum, running_min)
+
+
+        return ans
